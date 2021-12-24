954bef5c5eaca8d9791193c7c20b1485cb1c8b33
tancheng <tancheng1990@gmail.com>
2021-12-24 09:54:55 -0800

    [feature] enable conversion from float and double

--- bfloat16.hpp
+++ bfloat16.hpp
@@ -8,6 +8,15 @@ struct bfloat16 {
       data = 0;
     }
 
+    bfloat16(float t_floatVal) {
+      data = (*reinterpret_cast<unsigned int *>(&t_floatVal)) >> 16;
+    }
+
+    bfloat16(double t_doubleVal) {
+      float floatVal = (float)t_doubleVal;
+      data = (*reinterpret_cast<unsigned int *>(&floatVal)) >> 16;
+    }
+
     //cast to float
     operator float(){
       unsigned int extendedData = data << 16;
